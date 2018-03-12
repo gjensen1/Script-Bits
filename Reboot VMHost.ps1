23f663f47e091f520d58851f8d4b541ebbc5e964
gjensen1 <gjensen1@cogeco.ca>
2018-03-12 10:03:33 -0400

    Initial

---
+++ Reboot VMHost.ps1
@@ -0,0 +1,9 @@
+﻿Connect-VIServer 142.145.180.9 -Username root -Password "password"
+
+$vms = get-vm | where { $_.PowerState -eq "PoweredOn" }
+
+foreach ( $vm in $vms ) {Shutdown-VMGuest -VM $vm -Confirm:$false }
+
+sleep 60
+
+Restart-VMHost -Force -Confirm:$false
