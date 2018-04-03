88a779d980ec23237ee796c2f9572ee4a8a275a5
gjensen1 <gjensen1@cogeco.ca>
2018-04-03 11:13:01 -0400

    Initial

---
+++ CreateSnapShot.ps1
@@ -0,0 +1,155 @@
+﻿# +------------------------------------------------------+
+# |        Load VMware modules if not loaded             |
+# +------------------------------------------------------+
+"Loading VMWare Modules"
+$ErrorActionPreference="SilentlyContinue" 
+if ( !(Get-Module -Name VMware.VimAutomation.Core -ErrorAction SilentlyContinue) ) {
+    if (Test-Path -Path 'Registry::HKEY_LOCAL_MACHINE\SOFTWARE\WOW6432Node\VMware, Inc.\VMware vSphere PowerCLI' ) {
+        $Regkey = 'Registry::HKEY_LOCAL_MACHINE\SOFTWARE\WOW6432Node\VMware, Inc.\VMware vSphere PowerCLI'
+       
+    } else {
+        $Regkey = 'Registry::HKEY_LOCAL_MACHINE\SOFTWARE\VMware, Inc.\VMware vSphere PowerCLI'
+    }
+    . (join-path -path (Get-ItemProperty  $Regkey).InstallPath -childpath 'Scripts\Initialize-PowerCLIEnvironment.ps1')
+}
+$ErrorActionPreference="Continue"
+
+# -----------------------
+# Define Global Variables
+# -----------------------
+$Global:Folder = $env:USERPROFILE+"\Documents\CreateSnapshot" 
+$Global:MasterList = $null
+$Global:HostList = @()
+$Global:VCName = $null
+$Global:Creds = $null
+
+
+
+#*****************
+# Get VC from User
+#*****************
+Function Get-VCenter {
+    [CmdletBinding()]
+    Param()
+    #Prompt User for vCenter
+    Write-Host "Enter the FQHN of the vCenter to Get Hosting Listing From: " -ForegroundColor "Yellow" -NoNewline
+    $Global:VCName = Read-Host 
+}
+#*******************
+# EndFunction Get-VC
+#*******************
+
+#*************************************************
+# Check for Folder Structure if not present create
+#*************************************************
+Function Verify-Folders {
+    [CmdletBinding()]
+    Param()
+    "Building Local folder structure" 
+    If (!(Test-Path $Global:Folder)) {
+        New-Item $Global:Folder -type Directory
+  <#      New-Item "$Global:WorkFolder\Annotations" -type Directory
+        New-Item "$Global:WorkFolder\IPConfig" -type Directory
+        New-Item "$Global:WorkFolder\ShareInfo" -type Directory
+        New-Item "$Global:WorkFolder\VMInfo" -type Directory
+        New-Item "$Global:WorkFolder\PrinterInfo" -type Directory
+    #>
+        }
+    "Folder Structure built" 
+}
+#***************************
+# EndFunction Verify-Folders
+#***************************
+
+#*******************
+# Connect to vCenter
+#*******************
+Function Connect-VC {
+    [CmdletBinding()]
+    Param()
+    "Connecting to $Global:VCName"
+    Connect-VIServer $Global:VCName -Credential $Global:Creds -WarningAction SilentlyContinue
+}
+#***********************
+# EndFunction Connect-VC
+#***********************
+
+#*******************
+# Disconnect vCenter
+#*******************
+Function Disconnect-VC {
+    [CmdletBinding()]
+    Param()
+    "Disconnecting $Global:VCName"
+    Disconnect-VIServer -Server $Global:VCName -Confirm:$false
+}
+#**************************
+# EndFunction Disconnect-VC
+#**************************
+
+#**********************
+# Function Get-FileName
+#**********************
+Function Get-FileName {
+    [CmdletBinding()]
+    Param($initialDirectory)
+    [System.Reflection.Assembly]::LoadWithPartialName("System.windows.forms") | Out-Null
+    $OpenFileDialog = New-Object System.Windows.Forms.OpenFileDialog
+    $OpenFileDialog.initialDirectory = $initialDirectory
+    $OpenFileDialog.filter = "TXT (*.txt)| *.txt"
+    $OpenFileDialog.ShowDialog() | Out-Null
+    Return $OpenFileDialog.filename
+}
+#*************************
+# EndFunction Get-FileName
+#*************************
+
+#*************************
+# Function Read-TargetList
+#*************************
+Function Read-TargetList {
+    [CmdletBinding()]
+    Param($TargetFile)
+    $Targets = Get-Content $TargetFile
+    Return $Targets
+}
+#****************************
+# EndFunction Read-TargetList
+#****************************
+
+#**************
+# Take-Snapshot
+#**************
+Function Take-Snapshot($s) {
+    "Taking Snapshot of $s"
+    Get-VM $s | New-Snapshot -Name "Patching-Mar25" -Description "Snapshot before Virtual Hardware Upgrade"
+}
+#**************************
+# EndFunction Take-Snapshot
+#**************************
+
+
+#***************
+# Execute Script
+#***************
+CLS
+$ErrorActionPreference="SilentlyContinue"
+
+"=========================================================="
+" "
+Write-Host "Get CIHS credentials" -ForegroundColor Yellow
+$Global:Creds = Get-Credential -Credential $null
+
+Get-VCenter
+Connect-VC
+"=========================================================="
+"Get Target List"
+$inputFile = Get-FileName $Global:Folder
+"=========================================================="
+"Reading Target List"
+$VMHostList = Read-TargetList $inputFile
+"=========================================================="
+
+foreach ($VM in $VMHostList) {
+    Take-Snapshot $VM
+}
